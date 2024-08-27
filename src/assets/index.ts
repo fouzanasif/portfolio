f7a0d8f30c7bce141e66defea81b67f1b61a63ff
Fouzan Asif <160420253+fouzanasif@users.noreply.github.com>
2024-08-27 22:14:26 +0500

    Images used in portfolio uploaded

---
+++ src/assets/index.ts
@@ -0,0 +1,117 @@
+import logo from "./logo.png";
+import backend from "./Python.png";
+import creator from "./Azure.png";
+import mobile from "./NET.png";
+import web from "./angular.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+// import SQL from "./SQL.png";
+import playwright from "./Playwright.png";
+import tensorflow from "./tensorflow.png";
+import seo from "./seo.png";
+import powershell from "./powershell.png";
+import systems from "./company/systems.png";
+import acm from "./company/acm.png";
+import weeblog from "./company/weeblog.png";
+import upwork from "./company/upwork.png";
+import fiverr from "./company/fiverr.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import jmeter from "./tech/JMeter.png";
+import postman from "./tech/postman.png";
+import Selenium from "./tech/Selenium.png";
+import Jira from "./tech/jira.png";
+import SQL from "./tech/SQL.png";
+import Papyrus from "./tech/Papyrus.png";
+import AWS from "./tech/AWS.png";
+import overleaf from "./tech/overleaf.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide             from "./tripguide.png";
+import LMS                   from "./LMS.png";
+import Hospital              from "./Hospital.png";
+import Financial             from "./Financial.png";
+import FaceRecognition       from "./FaceRec.png";
+import Allure                from "./Allure.png";
+import objDet                from "./objDet.png";
+import OMS                   from "./OMS.png";
+import ParticipantsManager   from "./ParticipantsManager.png";
+import GroundMotion          from "./peerNgaRecords.png";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  playwright,
+  tensorflow,
+  seo,
+  powershell,
+  systems,
+  acm,
+  weeblog,
+  upwork,
+  fiverr,
+  jmeter,
+  postman,
+  Selenium,
+  AWS,
+  SQL,
+  Jira,
+  Papyrus,
+  overleaf,
+  LMS                ,
+  Hospital           ,
+  Financial          ,
+  FaceRecognition    ,
+  Allure             ,
+  objDet             ,
+  OMS                ,
+  ParticipantsManager,
+  GroundMotion       ,
+};
+
